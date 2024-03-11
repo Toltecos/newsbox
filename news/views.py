7180bf453c1e7a11fa7b6b02b068185e9a054d6a
Andrii Mogush <toltecos@ukr.net>
2024-03-11 19:00:14 +0200

    Create app news views and urls in views.py and urls.py

--- news/views.py
+++ news/views.py
@@ -1,3 +1,135 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from news.forms import NewspaperForm, RedactorCreationForm, RedactorUpdateForm
+from news.models import Newspaper, Topic
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    travel_topic_id = 7
+    last_ten_news = Newspaper.objects.exclude(topic=travel_topic_id)[:7]
+
+    topics = Topic.objects.all()
+    topics_last_news = {
+        topic.name.lower(): Newspaper.objects.filter(topic=topic.id)[:3]
+        for topic in topics
+    }
+    context = {
+        "last_ten_news": last_ten_news,
+        "topics_last_news": topics_last_news,
+    }
+    return render(request, "news/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    paginate_by = 10
+
+
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("news:topic-list")
+
+
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("news:topic-list")
+
+
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("news:topic-list")
+
+
+class NewspaperListView(generic.ListView):
+    model = Newspaper
+    context_object_name = 'news_list'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = Newspaper.objects.select_related("topic")
+        search = self.request.GET.get("search")
+        if search:
+            return queryset.filter(
+                Q(title__icontains=search) | Q(content__icontains=search)
+            )
+        return queryset
+
+
+class TopicNewspaperListView(generic.ListView):
+    model = Newspaper
+    context_object_name = 'news_list'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        topic_id = self.kwargs.get("topic_pk")
+        context["topic_id"] = topic_id
+        return context
+
+    def get_queryset(self):
+        topic_id = self.kwargs.get("topic_pk")
+        return Newspaper.objects.select_related("topic").filter(topic=topic_id)
+
+
+class NewspaperDetailView(generic.DetailView):
+    model = Newspaper
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        related_news = Newspaper.objects.filter(topic=self.object.topic.id)[:5]
+        context["related_news"] = related_news
+        return context
+
+
+class NewspaperCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Newspaper
+    form_class = NewspaperForm
+    success_url = reverse_lazy("news:news-list")
+
+
+class NewspaperUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Newspaper
+    form_class = NewspaperForm
+
+    def get_success_url(self):
+        return reverse_lazy("news:news-detail", kwargs={"pk": self.object.pk})
+
+
+class NewspaperDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Newspaper
+    success_url = reverse_lazy("news:news-list")
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = get_user_model()
+    paginate_by = 5
+
+
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
+    model = get_user_model()
+
+
+class RedactorCreateView(LoginRequiredMixin, generic.CreateView):
+    model = get_user_model()
+    form_class = RedactorCreationForm
+    success_url = reverse_lazy("news:redactor-list")
+
+
+class RedactorUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = get_user_model()
+    form_class = RedactorUpdateForm
+    success_url = reverse_lazy("news:redactor-list")
+
+
+class RedactorDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = get_user_model()
+    success_url = reverse_lazy("")

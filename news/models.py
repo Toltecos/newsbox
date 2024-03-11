de0714d12058cd2e1ddb6b0aa29c91fa6ddedd8f
Andrii Mogush <toltecos@ukr.net>
2024-03-11 18:45:06 +0200

    Create app models in news/models.py

--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="news")
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="news")
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username

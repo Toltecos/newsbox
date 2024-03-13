be3fea22eb81c3a3f12c702a6e4f89964c7cf401
Andrii Mogush <toltecos@ukr.net>
2024-03-13 20:18:30 +0200

    Change DEBUG in settings.py

--- newsbox/settings.py
+++ newsbox/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.environ.get("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "newsbox-gj5p.onrender.com"]
 

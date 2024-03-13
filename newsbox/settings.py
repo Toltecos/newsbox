fc330ff8cd53a5ced1c5edfc628b0adb858022a0
Andrii Mogush <toltecos@ukr.net>
2024-03-13 18:45:32 +0200

    Fix ALLOWED_HOSTS in settings.py

--- newsbox/settings.py
+++ newsbox/settings.py
@@ -152,8 +152,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # # This setting informs Django of the URI path from which your static files will be served to users
 # # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-# STATIC_URL = '/static/'
-#
+
 # # This production code might break development mode, so we check whether we're in DEBUG mode
 # if not DEBUG:
 #     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
